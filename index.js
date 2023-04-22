d9361d4bbbdb87e57cf00f0f44f1a3aebba6b8de
rscavuslu <samet.cavuslu@hotmail.com>
2023-04-22 22:55:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input = document.getElementById("button")
+
 function addingEventListener() {
+    input.addEventListener("click", addingEventListener);
+    alert('I was clicked!');
 }
+
+addingEventListener(input);
